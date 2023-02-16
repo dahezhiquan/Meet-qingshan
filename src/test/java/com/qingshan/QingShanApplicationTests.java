b6ec2fbd4d67962f4df59b0747200bf293a21e68
dahezhiquan <3390205563@qq.com>
2023-02-16 14:49:49 +0800

    最后修改

--- src/test/java/com/qingshan/QingShanApplicationTests.java
+++ src/test/java/com/qingshan/QingShanApplicationTests.java
@@ -4,7 +4,6 @@ import com.qingshan.utils.RedisIdWorker;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.annotation.Resource;
